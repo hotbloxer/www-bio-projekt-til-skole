2edf0d05db3c625f6f5c0b104d716e581d9a1d71
nhglud <nhglud@gmail.com>
2025-03-25 13:18:55 +0100

    film grid

--- load_movies.js
+++ load_movies.js
@@ -1,24 +1,37 @@
-
 loadMovies();
 
 async function loadMovies() {
     const response = await fetch("data/film_liste.json");
     const obj = await response.json();
 
-    let movieGrid = document.getElementById('movies');
+    let movieGrid = document.getElementById('all-movies');
 
-    obj.movies.forEach(movie => {
-        let e = document.createElement('div');
-        let fig = document.createElement('figure');
-        let img = document.createElement('img');
-        let figcaption = document.createElement('figcaption');
+    obj.Movies.forEach(movie => {
+        let movieContent = document.createElement('div');
+        let image = document.createElement('img');
+        let title = document.createElement('div');
+        let button = document.createElement('button');
+        
+        movieContent.className = "movie-content";
+        button.textContent = "Køb billet";
+        image.src = movie["Image"];
+        title.textContent = movie["Original Title"];
 
-        figcaption.textContent = movie["Original Title"];
-        img.src = movie["Image"];
+        movieContent.appendChild(image);
+        movieContent.appendChild(title);
+        movieContent.appendChild(button);
+        movieGrid.appendChild(movieContent);
 
-        fig.appendChild(img);
-        fig.appendChild(figcaption);
-        e.appendChild(fig);
-        movieGrid.appendChild(e);
+        // let fig = document.createElement('figure');
+        // let img = document.createElement('img');
+        // let figcaption = document.createElement('figcaption');
+
+        // figcaption.textContent = movie["Original Title"];
+        // img.src = movie["Image"];
+
+        // fig.appendChild(img);
+        // fig.appendChild(figcaption);
+        // e.appendChild(fig);
+        // movieGrid.appendChild(e);
     });
 }
