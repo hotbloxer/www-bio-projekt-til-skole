d992bc2ed9c0d13e0abcaf20bd2e1a7befe2b1cc
hotbloxer <33450949+hotbloxer@users.noreply.github.com>
2025-03-26 14:27:14 +0100

    bio update

--- javascript/seats-generator.js
+++ javascript/seats-generator.js
@@ -11,7 +11,8 @@ function fillGrid  (id, rows, colums) {
     for (let r = 0; r<rows; r++) {
         
         for (let c = 0; c < colums; c++) {
-            constructElement += constructSeat(true);
+            constructElement += constructSeat(0, r, c);
+            
         }
     }
 
@@ -21,11 +22,53 @@ function fillGrid  (id, rows, colums) {
     idTag.innerHTML = constructElement;
 }
 
-function constructSeat (free) {
-
-    if (free) {
-        return `<div class = "seat seat-free"> d</div>`
+//status 0 free, 1 selected, 2 reserved
+function constructSeat (status, r,c) {
+    let id = "r" + r + "c" + c;
+    if (status == 0) {
+        return `
+        <button onclick = "rowClicked(${id})" id = ${id} class = "seat seat-free"> d</button>
+        `   
     }
+
+    if (status == 1) {
+        return `
+        <button onclick = "rowClicked(${id})" id = ${id} class = "seat seat-selected"> d</button>
+        `  
+    }
+
     return `<div class = "seat seat-reserved"> d</div>`
 }
 
+function setAvailability (free) {
+    
+}
+
+function rowClicked (id) {
+
+    console.log( id.id);
+
+    let change = document.getElementById(id.id);
+
+    var rNumber = "";
+    var cNumber = "";
+
+    for (let char of id.id) {
+        if (char === 'c') {
+            foundC = true;
+        } else if (char === 'r') {
+            foundC = false;
+        } else if (!isNaN(char)) {
+            if (foundC) {
+                cNumber += char;
+            } else {
+                rNumber += char;
+            }
+        }
+    }
+
+
+    change.outerHTML = constructSeat(1, rNumber,cNumber);
+    //change.innerHTML = `<div class = "seat seat-reserved"> d</div>`;
+
+}
