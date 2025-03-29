dc7bbc377ebbff4f262b3bf64e959785fd7ab578
nhglud <nhglud@gmail.com>
2025-03-29 17:36:32 +0100

    refactoring procrastination

--- load_movies.js
+++ load_movies.js
@@ -13,30 +13,33 @@ function fillMovieGrid(obj) {
     let movieGrid = document.getElementById('all-movies');
 
     obj.Movies.forEach(movie => {
-        let movieContent = document.createElement('div');
-        let image = document.createElement('img');
-        let title = document.createElement('div');
-        let button = document.createElement('button');
-        
-        movieContent.className = "movie-content";
-
-        image.src = movie["Image"];
-        image.alt = movie["Original Title"];
-        image.title = movie["Original Title"];
-
-        title.textContent = movie["Original Title"];
-        
-        button.textContent = "Vælg film";
-        button.onclick = () => {
-            sessionStorage.setItem("SelectedMovie", JSON.stringify(movie));
-            window.location = "movieinfo.html";
-        } 
-
-        movieContent.appendChild(image);
-        movieContent.appendChild(title);
-        movieContent.appendChild(button);
+        let movieContent = fillMovieContent(movie);
         movieGrid.appendChild(movieContent);
     });
+}
+
+function fillMovieContent(movie) {
+    let movieContent = document.createElement('div');
+    let image = document.createElement('img');
+    let title = document.createElement('div');
+    let button = document.createElement('button');
+    
+    movieContent.className = "movie-content";
+
+    image.src = movie["Image"];
+    image.alt = movie["Original Title"];
+    image.title = movie["Original Title"];
+
+    title.textContent = movie["Original Title"];
+    
+    button.textContent = "Vælg film";
+    button.onclick = () => selectMovie(movie);
+
+    movieContent.appendChild(image);
+    movieContent.appendChild(title);
+    movieContent.appendChild(button);
+
+    return movieContent;
 }
 
 function fillHeroSquares(obj) {
@@ -45,33 +48,38 @@ function fillHeroSquares(obj) {
     for(let heroSquare of heroSquares) {
         let index = getRandomInt(obj.Movies.length);
         let movie = obj.Movies[index];
-        
-        let bigPic = document.createElement('div');
-        let img = document.createElement('img');
-        let heroAction = document.createElement('div');
-        let title = document.createElement('h1');
-        let button = document.createElement('button');
-
-        img.src = movie["Big Image"]; // TODO add big pictures
-        img.alt = movie["Original Title"];
-        img.title = movie["Original Title"];
-
-        heroAction.className = "hero-action";
-        title.textContent = movie["Original Title"];
-
-        button.textContent = "Vælg film";
-        button.onclick = () => {
-            sessionStorage.setItem("SelectedMovie", JSON.stringify(movie));
-            window.location = "movieinfo.html";
-        }
-
-        bigPic.appendChild(img);
-        heroAction.appendChild(title);
-        heroAction.appendChild(button);
-
-        heroSquare.appendChild(bigPic);
-        heroSquare.appendChild(heroAction);
+        fillHeroSquare(heroSquare, movie);
     }
+}
+
+function fillHeroSquare(heroSquare, movie) {
+    let bigPic = document.createElement('div');
+    let img = document.createElement('img');
+    let heroAction = document.createElement('div');
+    let title = document.createElement('h1');
+    let button = document.createElement('button');
+
+    img.src = movie["Big Image"]; 
+    img.alt = movie["Original Title"];
+    img.title = movie["Original Title"];
+
+    heroAction.className = "hero-action";
+    title.textContent = movie["Original Title"];
+
+    button.textContent = "Vælg film";
+    button.onclick = () => selectMovie(movie);
+
+    bigPic.appendChild(img);
+    heroAction.appendChild(title);
+    heroAction.appendChild(button);
+
+    heroSquare.appendChild(bigPic);
+    heroSquare.appendChild(heroAction);
+}
+
+function selectMovie(movie) {
+    sessionStorage.setItem("SelectedMovie", JSON.stringify(movie));
+    window.location = "movieinfo.html";
 }
 
 function getRandomInt(max) {
