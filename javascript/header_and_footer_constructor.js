1fa5d33d3aaa02130777d4ccb35dcfe4d48e175a
hotbloxer <33450949+hotbloxer@users.noreply.github.com>
2025-03-31 20:40:18 +0200

    footer

--- javascript/header_and_footer_constructor.js
+++ javascript/header_and_footer_constructor.js
@@ -1,7 +1,11 @@
 
 function setHeaderAndFooter () {
     let head = document.getElementById("headeren");
+    let foot = document.getElementById("footer-id");
+
+    
     head.innerHTML = constructHeader();
+    foot.innerHTML = constructFooter();
 }
 
 
@@ -18,5 +22,22 @@ function constructHeader () {
       `
 }
 
+
+
+
+
+function constructFooter () {
+    return `
+        <h3> Filmhistorie i Danmarks klasse</h3>
+        <p>Dansk film på lærredet, hygge i sædet!</p>
+        <div class = "footer-grid">
+
+            <a href="about.html">  OM OS</a>
+            <a href="contact.html"> KONTATK</1>
+            <a href="index.html"> FORSIDE</1>
+
+        </div>
+      `
+}
 
 
